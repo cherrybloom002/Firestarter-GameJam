15c21ebb2e53a707b604184ad3d374f478002ee5
JacKRcC <jacopo1405@gmail.com>
2025-09-30 12:24:05 +0200

    (wip) flashlight
    -imported flashlight assets
    -made BP_FlashLight
    -added flashlight socket

--- Fagottino/Intermediate/Build/Win64/UnrealEditor/Inc/Fagottino/UHT/Fagottino.init.gen.cpp
+++ Fagottino/Intermediate/Build/Win64/UnrealEditor/Inc/Fagottino/UHT/Fagottino.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeFagottino_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xCA7D24F8,
+				0xD6B2AF8C,
 				0xA67F6155,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -29,5 +29,5 @@ void EmptyLinkFunctionForGeneratedCodeFagottino_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_Fagottino.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Fagottino(Z_Construct_UPackage__Script_Fagottino, TEXT("/Script/Fagottino"), Z_Registration_Info_UPackage__Script_Fagottino, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xCA7D24F8, 0xA67F6155));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Fagottino(Z_Construct_UPackage__Script_Fagottino, TEXT("/Script/Fagottino"), Z_Registration_Info_UPackage__Script_Fagottino, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD6B2AF8C, 0xA67F6155));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
